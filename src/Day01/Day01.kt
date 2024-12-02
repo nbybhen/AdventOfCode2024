879d2cc1d386196c5e1f62082eda5c9a765e341a
ExplosiveEnd <henockdinberu11@gmail.com>
2024-12-01 20:19:54 -0500

    Updated Day-01

--- src/Day01/Day01.kt
+++ src/Day01/Day01.kt
@@ -1,46 +1,28 @@
 package Day01
+import getTwoColumns
 import java.io.File
 import kotlin.math.abs
 
-fun Part1(input: List<String>): Int {
-    val left = mutableListOf<Int>()
-    val right = mutableListOf<Int>()
-    val result = mutableListOf<Int>()
-
-    input.forEach {
-        val split = it.split(" ").filter { i -> i.isNotBlank() }.map { i -> i.toInt() }
-        left.add(split.first())
-        right.add(split.last())
-    }
-    left.sort()
-    right.sort()
-
-    for (i in 0..<left.size) {
-        result.add(abs(left[i] - right[i]))
+fun part1(input: List<String>): Int {
+    val columns = getTwoColumns(input).also {
+        it.first.sort()
+        it.second.sort()
     }
 
-    return result.sum()
+    return run {
+        columns.first.zip(columns.second).fold(0) {acc, (f, s) -> acc + abs(f - s)}
+    }
 }
 
-fun Part2(input: List<String>): Int {
-    val left = mutableListOf<Int>()
-    val right = mutableMapOf<Int, Int>()
-    val result = mutableListOf<Int>()
-
-    input.forEach {
-        val split = it.split(" ").filter { i -> i.isNotBlank() }.map { i -> i.toInt() }
-        left.add(split.first())
-        right[split.last()] = right.getOrDefault(split.last(), 0) + 1
+fun part2(input: List<String>): Int {
+    return getTwoColumns(input).let {
+        val occ = it.second.groupingBy{inner -> inner}.eachCount()
+        it.first.fold(0) {acc, ele -> acc + ele * occ.getOrDefault(ele, 0)}
     }
-
-    left.forEach {
-        result.add(it * right.getOrDefault(it, 0))
-    }
-    return result.sum()
 }
 
 fun main() {
     val input = File("src/Day01/input.txt").inputStream().bufferedReader().readLines()
-    println("Part 1: ${Part1(input)}")
-    println("Part 2: ${Part2(input)}")
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
 }
---
+++ src/Lib.kt
@@ -0,0 +1,15 @@
+// Common Functions
+
+// Returns a Pair of MutableLists .first = 1st column and .second = 2nd column
+fun getTwoColumns(input: List<String>): Pair<MutableList<Int>, MutableList<Int>> {
+    val left = mutableListOf<Int>()
+    val right = mutableListOf<Int>()
+
+    input.forEach {
+        val split = it.split(" ").filter { i -> i.isNotBlank() }.map { i -> i.toInt() }
+        left.add(split.first())
+        right.add(split.last())
+    }
+
+    return left to right
+}
